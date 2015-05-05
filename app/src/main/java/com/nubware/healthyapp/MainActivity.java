baed4fc1eac257f252b7f12c52361a938ac31e0f
colornw <color@nubware.com>
2015-05-05 09:29:41 -0600

    060415

--- app/src/main/java/com/nubware/healthyapp/MainActivity.java
+++ app/src/main/java/com/nubware/healthyapp/MainActivity.java
@@ -15,9 +15,9 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteHelper sqlHelper = new SQLiteHelper(getApplicationContext());
+        //SQLiteHelper sqlHelper = new SQLiteHelper(getApplicationContext());
 
-        sqlHelper.create();
+        //sqlHelper.create();
     }
 
 
